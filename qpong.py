15b71f3f666fbbedfc32b1becd33312e3b040db8
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-03-30 17:12:05 -0400

    Adds a circuit_grid to the pygame

--- qpong.py
+++ qpong.py
@@ -1,11 +1,17 @@
 import pygame
 
+from assets.circuit_grid import CircuitGrid
+from assets import globals
+
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
 pygame.display.set_caption('QPong')
 clock = pygame.time.Clock()
 
 def main():
+    #initialize the game
+    circuit_grid = CircuitGrid(5, globals.FIELD_HEIGHT)
+
 
     exit = False
     while not exit:
@@ -13,6 +19,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
+
+        # update game section - happens every frame can be handling some inputs or character moving
+
+        # draw game section 
+        circuit_grid.draw(screen)
+        pygame.display.flip()
 
         # Set the framerate - 60 frame per second game.
         clock.tick(60)
